ebdeb775a3e346b632c4906a4b19370794352c67
kabrate <937160666@qq.com>
2018-03-07 22:39:09 +0800

    finish!

--- 2015/磊骰子2.cpp
+++ 2015/磊骰子2.cpp
@@ -8,7 +8,8 @@ map<int, vector<int> > mp;
 int n, m;//骰子数目 有冲突的面的数量 
 int num1, num2;
 //map<int, int> mp2;
-map<int, vector<int >> mp2;
+map<int, vector<int> > mp2;
+//map<long long, int (*)[6]> d;
 long long d[5][6];
 long long sum;
 int oppsite(int i)
@@ -32,7 +33,12 @@ int main()
 		mp2[num1].push_back(num2);
 		mp2[num2].push_back(num1);
 	}
-	d[1][1] = d[1][2] = d[1][3] = d[1][4] = d[1][5] = d[1][6] = 1;
+	for(int i=0;i<6;i++)
+	{
+		d[1][i]=1;
+	}
+	
+	//d[1][1] = d[1][2] = d[1][3] = d[1][4] = d[1][5] = d[1][6] = 1;
 	for (int i = 2; i <= n; i++)//第几个骰子 
 	{
 		for (int j = 1; j <= 6; j++)//骰子向上的面 
@@ -41,12 +47,13 @@ int main()
 			{
 				if (find(mp2[oppsite(j)].begin(),mp2[oppsite(j)].end(),t)==mp2[oppsite(j)].end())//不冲突 
 				{
-					d[i][j] += d[(i - 1)][t];//上面骰子的情况等于下面不跟它冲突的骰子情况数量相加
+					d[i][j-1] += d[(i - 1)][t-1];//上面骰子的情况等于下面不跟它冲突的骰子情况数量相加
+					d[i-1][t-1]=0;
 				}
 			}
 		}
 	}
-	sum = d[n%3][1] + d[n%3][2] + d[n][3] + d[n][4] + d[n][5] + d[n][6];
+	sum = d[n][0] + d[n][1] + d[n][2] + d[n][3] + d[n][4] + d[n][5];
 	sum = sum*pow(4, n);
 	sum = sum % 1000000007;
 	printf("%ld", sum);
