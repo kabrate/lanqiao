6b17063b722e4cbba20b1581b77d4e1a87a16ad4
kabrate <937160666@qq.com>
2018-03-10 23:44:28 +0800

    da lao xie de

--- 2015/生命之树.cpp
+++ 2015/生命之树.cpp
@@ -1,64 +1,40 @@
-#include "stdafx.h"
-#include<cstdio>
-#include<vector>
-#include<map>
-#include<algorithm>
-using namespace std;
-map <int, vector<int> > mp;
-vector<int> v;
-int n, num, num1, num2;
-int front, behind,ed;//前一个节点 后一个节点 
-int ans;//答案 
-vector<int> visit;
-void dfs(int front)
-{
-	if (front == ed)
-	{
-		return;
-	}
-	for (int t = 0; t < mp[front].size(); t++)//找前一个节点所连的未访问的节点
-	{
-		visit[front] = 1;
-		behind = mp[front][t];
-		if (visit[behind] == 1)//如果下一个节点已经访问直接跳过
-		{
-			continue;
-		}	
-		dfs(behind);
-		visit[behind] = 0;
-	}
-}
-int compare(int i, int j)
-{
-	ed = j;
-	dfs(i);
-	return i;
-}
-int main()
-{
-	scanf_s("%d", &n);
-	for (int i = 0; i<n; i++)
-	{
-		scanf_s("%d", &num);
-		v.push_back(num);
-		visit.push_back(0);
-	}
-	for (int i = 0; i<n - 1; i++)
-	{
-		scanf_s("%d %d", &num1, &num2);
-		mp[num1].push_back(num2);
-		mp[num2].push_back(num1);
-	}
-	for (int i = 0; i<n; i++)
-	{
-		for (int j = 1; j<n ; j++)
-		{
-			compare(i,j);
-		}
-	}
-	return 0;
-}
-
-
-
-
+ #include<iostream>  
+ #include<algorithm>  
+ #include<cstdio>  
+ #include<vector>  
+using namespace std;  
+typedef long long LL;  
+const int MAXN=100000+10;  
+const int INF=-1000000-10;  
+LL dp[MAXN][2];  
+vector<int>tree[MAXN];  
+LL w[MAXN];  
+  
+void dfs(int p,int fa){  
+    dp[p][0]=max(dp[p][0],w[p]);  
+    dp[p][1]=max(dp[p][1],w[p]);  
+    for(int i=0;i<tree[p].size();i++){  
+        int son=tree[p][i];  
+        if(son^fa){  
+            dfs(son,p);  
+            dp[p][0]=max(dp[p][0],dp[son][0]);  
+            if(dp[son][1]>0)dp[p][1]+=dp[son][1];  
+        }  
+    }  
+    dp[p][0]=max(dp[p][0],dp[p][1]);  
+}  
+  
+int main(){  
+    int n,a,b;  
+    cin>>n;  
+    for(int i=1;i<=n;i++)cin>>w[i];  
+    for(int i=1;i<=n;i++){dp[i][0]=INF;dp[i][1]=INF;}  
+    for(int i=1;i<n;i++){  
+        cin>>a>>b;  
+        tree[a].push_back(b);  
+        tree[b].push_back(a);  
+    }  
+    dfs(1,-1);  
+    cout<<dp[1][0]<<"\n";  
+    return 0;  
+} 
